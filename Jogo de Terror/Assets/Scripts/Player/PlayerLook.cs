e27638d24b8f2e77c5bb6fad967312053d540455
Manoel Scheidt Ender <manoel.ender@alunos.sc.senac.br>
2026-06-16 09:07:32 -0300

    Player
    Anda
    Pula
    Primeira pessoa

--- Jogo de Terror/Assets/Scripts/Player/PlayerLook.cs
+++ Jogo de Terror/Assets/Scripts/Player/PlayerLook.cs
@@ -1,16 +1,57 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerLook : MonoBehaviour
+public sealed class PlayerLook : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Transform playerBody;
+    [SerializeField] private float sensitivity = 0.2f;
+
+    private InputSystem_Actions inputActions;
+    private Vector2 lookInput;
+    private float verticalRotation;
+
+    private void Awake()
     {
-        
+        inputActions = new InputSystem_Actions();
+
+        inputActions.Player.Look.performed += context =>
+            lookInput = context.ReadValue<Vector2>();
+
+        inputActions.Player.Look.canceled += _ =>
+            lookInput = Vector2.zero;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+    private void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        if (Cursor.lockState != CursorLockMode.Locked)
+            return;
+
+        Vector2 mouseDelta = lookInput * sensitivity;
+
+        verticalRotation -= mouseDelta.y;
+        verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f);
+
+        transform.localRotation =
+            Quaternion.Euler(verticalRotation, 0f, 0f);
+
+        playerBody.Rotate(
+            Vector3.up * mouseDelta.x
+        );
     }
 }
